66a174690ed5bda4e580cf20f3fa4ec6e74fa5c9
mainrdsniper09 <barretosanchez6@gmail.com>
2025-02-08 18:30:59 -0500

    Add Product model with category and quantity fields (generado por copilot)

--- home/dashboard/models.py
+++ home/dashboard/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+# Base de datos de productos
+
 # Create your models here.
+
+CATEGORY = (
+    ('Celulares', 'Celulares'),
+    ('Computadores', 'Computadores'),
+    ('Televisores', 'Televisores'),
+)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY,null=True)
+    quantity = models.PositiveIntegerField(null=True)
+
