c6a040cddc34e1045b720fecfece105033a61806
mainrdsniper09 <barretosanchez6@gmail.com>
2025-02-09 11:18:31 -0500

    Register Order model and enhance Product admin interface with custom display and filtering options

--- home/dashboard/admin.py
+++ home/dashboard/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Product # Importamos el modelo Product
+from .models import Product,Order # Importamos el modelo Product y Order
+from django.contrib.auth.models import Group # Importamos el modelo Group
+
+admin.site.site_header = "Administracion" # Cambiamos el nombre del panel administrativo
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'quantity') # Indicamos los campos que queremos mostrar en el panel administrativo
+    list_filter = ['category',] # Indicamos los campos que queremos filtrar en el panel administrativo
 
 # Register your models here.
 
-admin.site.register(Product) # Registramos el modelo Product en nuestro panel administrativo
+admin.site.register(Product, ProductAdmin) # Registramos el modelo Product en nuestro panel administrativo
+admin.site.register(Order) # Registramos el modelo Order en nuestro panel administrativo
+
+# admin.site.unregister(Group) # Desregistramos el modelo Group en nuestro panel administrativo
