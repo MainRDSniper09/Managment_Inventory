17dea389f87f2b3a78bb52b5ffc75d980fad96f0
mainrdsniper09 <barretosanchez6@gmail.com>
2025-02-08 20:07:12 -0500

    Register Product model in admin panel

--- home/dashboard/admin.py
+++ home/dashboard/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Product # Importamos el modelo Product
 
 # Register your models here.
+
+admin.site.register(Product) # Registramos el modelo Product en nuestro panel administrativo
