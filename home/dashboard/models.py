f08b70915e452064c9fbab82580c29dda9090c98
mainrdsniper09 <barretosanchez6@gmail.com>
2025-02-09 11:18:36 -0500

    Add Order model and enhance Product model with verbose names and string representation

--- home/dashboard/models.py
+++ home/dashboard/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User # Importamos el modelo User
 
 # Base de datos de productos
 
@@ -15,5 +16,22 @@ class Product(models.Model):
     category = models.CharField(max_length=20, choices=CATEGORY,null=True)
     quantity = models.PositiveIntegerField(null=True)
 
+    class Meta: # Indicamos el nombre de la tabla
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
+
     def __str__(self):
         return f'{self.name}-{self.quantity}' # Retornamos el nombre del producto y la cantidad
+    
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    staff = models.ForeignKey(User, models.CASCADE, null=True)
+    order_quantity = models.PositiveIntegerField(null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta: # Indicamos el nombre de la tabla
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+    def __str__(self):
+        return f'{self.product} ordered by {self.staff.username}' # Retornamos el producto y el usuario que lo ordeno
