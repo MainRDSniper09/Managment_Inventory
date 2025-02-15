11403135ef430eda839e160289ae7198ced45018
MainRDSniper09 <barretosanchez6@gmail.com>
2025-02-14 21:10:51 -0500

    Se agrega nuevas URLs

--- home/dashboard/urls.py
+++ home/dashboard/urls.py
@@ -5,6 +5,7 @@ from . import views # Importamos views
 urlpatterns = [
     path('dashboard/', views.index, name='dashboard-index'), # Ruta principal
     path('staff/', views.staff, name='dashboard-staff'), # Ruta staff
+     path('staff/detail<int:pk>/', views.staff_detail, name='dashboard-staff-detail'), # Ruta perfil staff
     path('product/', views.product, name='dashboard-product'), # Ruta Productos
     path('product/delete/<int:pk>/', views.product_delete, name='dashboard-product-delete'), # Ruta para Productos eliminados
     path('product/update/<int:pk>/', views.product_update, name='dashboard-product-update'), # Ruta para Productos editados  
