4a30b5fdf814b998c435b094e040a37ba0dceb4e
MainRDSniper09 <barretosanchez6@gmail.com>
2025-02-14 21:10:03 -0500

    Se agrega logica para algunas vistas

--- home/dashboard/views.py
+++ home/dashboard/views.py
@@ -1,19 +1,48 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Product
-from .forms import ProductForm
+from .models import Product, Order
+from .forms import ProductForm, OrderForm
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 # Create your views here.
 
 
 @login_required # Se necesita loguearse para poder ver las demas rutas
 def index(request):
-    return render(request, 'dashboard/index.html')
+    orders = Order.objects.all()
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.staff = request.user
+            instance.save()
+            return redirect('dashboard-index')
+    else:
+        form = OrderForm()
+    context = {
+        'orders': orders,
+        'form' : form,
+    }
+    return render(request, 'dashboard/index.html', context)
 
 @login_required # Se necesita loguearse para poder ver las demas rutas
 def staff(request):
-    return render(request, 'dashboard/staff.html')
+    workers = User.objects.all()
+    context = {
+        'workers':workers
+    }
+    return render(request, 'dashboard/staff.html', context)
+
+@login_required
+def staff_detail(request, pk):
+    workers = User.objects.get(id=pk)
+    context ={
+        'workers': workers,
+    }  
+    return render(request, 'dashboard/staff_detail.html', context)
 
 @login_required # Se necesita loguearse para poder ver las demas rutas
 def product(request):
@@ -24,6 +53,9 @@ def product(request):
         form = ProductForm(request.POST)
         if form.is_valid():
             form.save()
+            product_name = form.cleaned_data.get('name')
+            messages.success(request, f'{product_name} fue añadido')
+            
             return redirect('dashboard-product')
     else:
         form = ProductForm()
@@ -35,6 +67,7 @@ def product(request):
     }
     return render(request, 'dashboard/product.html', context)
 
+@login_required
 def product_delete(request, pk): # Recibimos la llave primaria para eliminar
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -42,6 +75,7 @@ def product_delete(request, pk): # Recibimos la llave primaria para eliminar
         return redirect('dashboard-product')
     return render(request, 'dashboard/product_delete.html')
 
+@login_required
 def product_update(request, pk): # Metodo para modificar un producto
     item = Product.objects.get(id=pk)
     if request.method == 'POST':
@@ -58,6 +92,11 @@ def product_update(request, pk): # Metodo para modificar un producto
 
 @login_required # Se necesita loguearse para poder ver las demas rutas
 def order(request):
-    return render(request, 'dashboard/order.html')
+    orders = Order.objects.all()
+    
+    context = {
+        'orders': orders,
+    }
+    return render(request, 'dashboard/order.html', context)
 
 
