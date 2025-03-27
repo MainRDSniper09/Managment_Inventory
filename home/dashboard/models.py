055af689c57fac44301f51df47f0f1655c6d24dc
MainRDSniper09 <barretosanchez6@gmail.com>
2025-03-27 13:33:28 -0500

    Se arregla modelo Product

--- home/dashboard/models.py
+++ home/dashboard/models.py
@@ -13,15 +13,17 @@ CATEGORY = (
 
 class Product(models.Model):
     name = models.CharField(max_length=100, null=True)
-    category = models.CharField(max_length=20, choices=CATEGORY,null=True)
+    category = models.CharField(max_length=20, choices=CATEGORY, null=True)
     quantity = models.PositiveIntegerField(null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)  # Agregar este campo
 
-    class Meta: # Indicamos el nombre de la tabla
+    class Meta:
         verbose_name = 'Product'
         verbose_name_plural = 'Products'
 
     def __str__(self):
-        return f'{self.name}-{self.quantity}' # Retornamos el nombre del producto y la cantidad
+        return f'{self.name}-{self.quantity}'
+
     
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
